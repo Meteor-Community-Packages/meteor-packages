a5334c8b37d4af8fe7ec024722108f10309a51ea
Mitar <mitar.git@tnode.com>
2016-04-11 00:55:17 -0700

    Version bump.

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'peerlibrary:meteor-packages',
   summary: "Client for Meteor Package Server API",
-  version: '0.2.1',
+  version: '0.2.2',
   git: 'https://github.com/peerlibrary/meteor-packages.git'
 });
 
