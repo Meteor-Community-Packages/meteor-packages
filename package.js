8fa808dac47beabe0c154b68a5790a7c85d7a860
Kelly Copley <copleykj@gmail.com>
2020-01-30 15:29:29 -0500

    v2.0.1

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name: 'peerlibrary:meteor-packages',
   summary: 'Client for Meteor Package Server API',
-  version: '2.0.0',
+  version: '2.0.1',
   git: 'https://github.com/Meteor-Community-Packages/meteor-packages.git',
 });
 
