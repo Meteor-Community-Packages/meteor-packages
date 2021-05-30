603d0c0c0c2fbeda3ede576acc8bb017dcc52088
Kelly Copley <copleykj@gmail.com>
2021-05-30 13:51:43 -0400

    v2.1.2

--- package.js
+++ package.js
@@ -2,12 +2,12 @@
 Package.describe({
   name: 'peerlibrary:meteor-packages',
   summary: 'Client for Meteor Package Server API',
-  version: '2.1.1',
+  version: '2.1.2',
   git: 'https://github.com/Meteor-Community-Packages/meteor-packages.git',
 });
 
 Package.onUse(function (api) {
-  api.versionsFrom('1.6');
+  api.versionsFrom('1.8');
   api.use(['ecmascript', 'fetch', 'random', 'mongo', 'ddp', 'underscore', 'package-version-parser']);
   api.use('matb33:collection-hooks@0.8.0 || 1.0.0');
   api.mainModule('client.js', 'client');
